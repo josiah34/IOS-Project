a1f27f48b8c00892d33609f0486633d8d97090b3
Ravkeerat Singh <42888466+Ravkeerat02@users.noreply.github.com>
2023-04-10 13:58:42 -0400

    Due Date Cheecker

--- GoalGetter/Views/ItemDetailView.swift
+++ GoalGetter/Views/ItemDetailView.swift
@@ -12,6 +12,7 @@ struct ItemDetailView: View {
     
     @State private var title: String
     @State private var priority: Priority
+    @State private var dueDate: Date? // Add a state variable for the due date
     
     init(item: ItemModel) {
         self.item = item
@@ -19,6 +20,7 @@ struct ItemDetailView: View {
         // Initialize the state variables with the values from the item
         _title = State(initialValue: item.title)
         _priority = State(initialValue: item.priority)
+        _dueDate = State(initialValue: item.dueDate) // Initialize the due date state variable
     }
     
     var body: some View {
@@ -37,9 +39,17 @@ struct ItemDetailView: View {
             .pickerStyle(SegmentedPickerStyle())
             .padding()
             
+            // Due date picker
+            DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .padding()
+            
             // Save button
             Button("Save") {
-                let updatedItem = item.updateTitle(title).updatePriority(priority)
+                let updatedItem = item
+                    .updateTitle(title)
+                    .updatePriority(priority)
+                    .updateDueDate(dueDate) // Update the due date of the item
                 listViewModel.updateItem(item: updatedItem)
             }
             .padding()
